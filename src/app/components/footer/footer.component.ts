1b780e21bac952d9d74011e4a8bd90f8868b8bd3
Ana Carolina <carol00_10@hotmail.com>
2025-01-20 20:01:19 -0300

    create footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  footer=[
+    {
+      img1:'assets/icons/onu-icon.svg',
+      img2:'assets/images/VERDE_E_DO_MEIO_AMBIENTE_HORIZONTAL_FUNDO_CLARO.png',
+    }
+  ];
 }
