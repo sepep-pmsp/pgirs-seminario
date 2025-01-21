0f72daf0449edad60079260ac02eed43f9fb90fe
Ana Carolina <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-01-21 11:57:11 -0300

    Programacao (#3)
    * create routes
    
    * Update styles.scss
    
    * create page programacao
    
    * Update programacao-page.component.scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './components/pages/home/homepage/homepage.component';
 import { PalestrantesPageComponent } from './components/pages/palestrantes/palestrantes-page/palestrantes-page.component';
+import { ProgramacaoPageComponent } from './components/pages/programacao/programacao-page/programacao-page.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full' , component: HomepageComponent },
   { path: 'palestrantes', component: PalestrantesPageComponent },
+  { path: 'programacao', component: ProgramacaoPageComponent },
 ];
 
 @NgModule({
