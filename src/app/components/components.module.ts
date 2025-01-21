c4d7e5ba51724b774364ffdbd7766053028525b5
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-01-21 12:37:17 -0300

    create page live

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -7,6 +7,8 @@ import { RouterModule } from '@angular/router';
 import { PalestrantesPageComponent } from './pages/palestrantes/palestrantes-page/palestrantes-page.component';
 import { ProgramacaoPageComponent } from './pages/programacao/programacao-page/programacao-page.component';
 import { VideoPageComponent } from './pages/live/video-page/video-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
@@ -21,10 +23,12 @@ import { VideoPageComponent } from './pages/live/video-page/video-page.component
   ],
   imports: [
     CommonModule,
+    BrowserModule,
+    HttpClientModule,
     RouterModule,
     NgClass,
     NgFor
   ],
-  exports: [ HomepageComponent, NavigationComponent, FooterComponent, PalestrantesPageComponent,]
+  exports: [ HomepageComponent, NavigationComponent, FooterComponent, PalestrantesPageComponent, VideoPageComponent]
 })
 export class ComponentsModule { }
