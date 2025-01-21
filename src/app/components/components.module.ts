3adb1110baa02e1cbf8cbf7a5b250eca2bd3b184
Ana Carolina <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-01-20 22:23:26 -0300

    Palestrantes (#2)
    * criate routes
    
    * create page palestrantes

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule, NgClass } from '@angular/common';
+import { CommonModule, NgClass, NgFor } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomepageComponent } from './pages/home/homepage/homepage.component';
 import { RouterModule } from '@angular/router';
+import { PalestrantesPageComponent } from './pages/palestrantes/palestrantes-page/palestrantes-page.component';
 
 
 
@@ -12,12 +13,14 @@ import { RouterModule } from '@angular/router';
     NavigationComponent,
     FooterComponent,
     HomepageComponent,
+    PalestrantesPageComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
-    NgClass
+    NgClass,
+    NgFor
   ],
-  exports: [ HomepageComponent, NavigationComponent, FooterComponent,]
+  exports: [ HomepageComponent, NavigationComponent, FooterComponent, PalestrantesPageComponent,]
 })
 export class ComponentsModule { }
---
+++ src/app/components/pages/palestrantes/palestrantes-page/palestrantes-page.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-palestrantes-page',
+  standalone: false,
+  
+  templateUrl: './palestrantes-page.component.html',
+  styleUrl: './palestrantes-page.component.scss'
+})
+export class PalestrantesPageComponent {
+  palestrante = [
+    {
+      
+      img:'',
+      title2:'Palestrante Z',
+      title4:'Cargo',
+      title5:'Ocupação',
+      descripiton:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend aliquam augue vitae dapibus. Aenean eu velit eget est faucibus imperdiet ut ac nunc. In eleifend, ligula quis accumsan consectetur cteturconsectetur ligula quis Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend aliquam augue vitae dapibus. Aenean eu velit eget est faucibus imperdiet ut ac nunc. In eleifend, ligula quis accumsan consectetur cteturconsectetur ligula quis Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend aliquam augue vitae dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend aliquam augue vitae dapibus. Aenean eu velit eget est faucibus imperdiet ut ac nunc. In eleifend, ligula quis accumsan consectetur cteturconsectetur ligula quis Lorem i',
+    },
+    {
+      img:'',
+      title2:'Palestrante X',
+      title4:'Cargo',
+      title5:'Ocupação',
+      descripiton:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend aliquam augue vitae dapibus. Aenean eu velit eget est faucibus imperdiet ut ac nunc. In eleifend, ligula quis accumsan consectetur cteturconsectetur ligula quis Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend aliquam augue vitae dapibus. Aenean eu velit eget est faucibus imperdiet ut ac nunc. In eleifend, ligula quis accumsan consectetur cteturconsectetur ligula quis Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend aliquam augue vitae dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend aliquam augue vitae dapibus. Aenean eu velit eget est faucibus imperdiet ut ac nunc. In eleifend, ligula quis accumsan consectetur cteturconsectetur ligula quis Lorem i',
+    },
+    {
+      img:'',
+      title2:'Palestrante Y',
+      title4:'Cargo',
+      title5:'Ocupação',
+      descripiton:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend aliquam augue vitae dapibus. Aenean eu velit eget est faucibus imperdiet ut ac nunc. In eleifend, ligula quis accumsan consectetur cteturconsectetur ligula quis Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend aliquam augue vitae dapibus. Aenean eu velit eget est faucibus imperdiet ut ac nunc. In eleifend, ligula quis accumsan consectetur cteturconsectetur ligula quis Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend aliquam augue vitae dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend aliquam augue vitae dapibus. Aenean eu velit eget est faucibus imperdiet ut ac nunc. In eleifend, ligula quis accumsan consectetur cteturconsectetur ligula quis Lorem i',
+    }
+  ];
+}
