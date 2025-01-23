5e66d21fea8be2acebbff3f5d07840888bccca7c
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-01-23 10:01:00 -0300

    add imgs governo mobile

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: false,
-  
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
   isMenuOpen = false;
-  constructor(
-    private route: ActivatedRoute, 
+  constructor( 
     private router: Router) {}
   ngOnInit() {
     this.router.events.subscribe(() => {
