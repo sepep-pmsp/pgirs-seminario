06934541d28409aaee64147593a0b21d85c9f7b2
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-01-28 09:53:06 -0300

    corrigido bug da barra de menu

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -13,12 +13,23 @@ export class NavigationComponent {
   constructor(private router: Router) {}
 
   ngOnInit() {
+    // Fecha o menu ao navegar para outra rota
     this.router.events.subscribe(() => {
       this.closeMenu();
     });
   }
 
+  toggleMenu() {
+    // Alterna o estado do menu
+    this.isMenuOpen = !this.isMenuOpen;
+    const checkbox = document.getElementById('navi-toggle') as HTMLInputElement;
+    if (checkbox) {
+      checkbox.checked = this.isMenuOpen;
+    }
+  }
+
   closeMenu() {
+    // Fecha o menu
     this.isMenuOpen = false;
     const checkbox = document.getElementById('navi-toggle') as HTMLInputElement;
     if (checkbox) {
